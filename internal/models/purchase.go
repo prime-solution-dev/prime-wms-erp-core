ee83640e277da8b530654b8bfbc07fce4666ad4f
NapasIsSky <napas.k@craftsmanship.co>
2025-09-25 15:08:09 +0700

    delete unused code

--- internal/models/purchase.go
+++ internal/models/purchase.go
@@ -136,10 +136,6 @@ func (PurchaseItem) TableName() string {
 }
 
 // DTOs
-type CreatePOResponse struct {
-	ID string `json:"id"`
-}
-
 type CreatePOBigLotItemRequest struct {
 	PreItem              string  `json:"pre_item"`
 	ProductGroupType     string  `json:"product_group_type"`
