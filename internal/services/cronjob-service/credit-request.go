2d2f42034abd9915030046b7c36a3c5adbc7503f
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2025-11-27 00:20:45 +0700

    t

--- internal/services/cronjob-service/credit-request.go
+++ internal/services/cronjob-service/credit-request.go
@@ -101,10 +101,10 @@ func CreditRequestEffectiveDtm() (interface{}, error) {
 	creditRequestUpdate := []models.CreditRequest{}
 	for _, creditRequestValue := range creditRequest.CreditRequest {
 		if creditRequestValue.ExpireDtm != nil {
-			now := time.Now()
+			/*now := time.Now()
 			exp := creditRequestValue.ExpireDtm
 			if exp.Before(now) {
-				/* creditTransaction = append(creditTransaction, models.CreditTransaction{
+				 creditTransaction = append(creditTransaction, models.CreditTransaction{
 					TransactionCode: creditRequestValue.RequestCode,
 					TransactionType: creditRequestValue.RequestType,
 					Amount:          creditRequestValue.Amount,
@@ -134,9 +134,9 @@ func CreditRequestEffectiveDtm() (interface{}, error) {
 					RequestDate:                  creditRequestValue.RequestDate,
 					ActionDate:                   creditRequestValue.ActionDate,
 					IsAction:                     creditRequestValue.IsAction,
-				}) */
+				})
 
-			}
+			}*/
 		}
 		if creditRequestValue.EffectiveDtm != nil {
 			fmt.Println("Now:", time.Now().Format(time.RFC3339))
