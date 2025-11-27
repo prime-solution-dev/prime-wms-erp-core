837141e2381988aac922a102b9a8c0838912bddc
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2025-11-27 11:32:43 +0700

    t

--- internal/services/credit-service/update-credit-request.go
+++ internal/services/credit-service/update-credit-request.go
@@ -22,24 +22,69 @@ func UpdateCreditRequest(ctx *gin.Context, jsonPayload string) (interface{}, err
 	creditRequestValue := []models.CreditRequest{}
 	creditTransaction := []models.CreditTransaction{}
 	credit := []models.Credit{}
+	customerCode := []string{}
+	for _, credit := range req {
+		customerCode = append(customerCode, credit.CustomerCode)
+	}
 
+	requestDataGetCredit := map[string][]string{
+		"customer_code": customerCode,
+	}
+	jsonBytesGetCredit, err := json.Marshal(requestDataGetCredit)
+	if err != nil {
+		return nil, err
+	}
+
+	GetCreditRes, errGetCredit := GetCredit(ctx, string(jsonBytesGetCredit))
+	if errGetCredit != nil {
+		return nil, errGetCredit
+	}
+
+	mapCredit := map[string]models.Credit{}
+	mapCreditExtra := map[uuid.UUID]models.CreditExtra{}
+
+	for _, creditValue := range GetCreditRes.(ResultCredit).Credit {
+		mapCredit[creditValue.CustomerCode] = creditValue
+		for _, creditExtraValue := range creditValue.CreditExtra {
+			mapCreditExtra[creditValue.ID] = creditExtraValue
+		}
+
+	}
+	creditIDForDelete := []uuid.UUID{}
+	creditExtraIDForDelete := []uuid.UUID{}
 	for i := range req {
-		/* if req[i].Status == "REJECT" {
-			creditTransaction = append(creditTransaction, models.CreditTransaction{
-				TransactionCode: req[i].RequestCode,
-				TransactionType: req[i].RequestType,
-				Amount:          req[i].Amount,
-				AdjustAmount:    0,
-				EffectiveDtm:    req[i].EffectiveDtm,
-				ExpireDtm:       req[i].ExpireDtm,
-				//ForceExpireDtm:  req[i].e,
-				//ApproveDate:     "",
-				IsApprove: false,
-				Status:    "REJECT",
-				Reason:    "",
-			})
+		if req[i].Status == "REJECT" {
+			if len(GetCreditRes.(ResultCredit).Credit) > 0 {
+				if req[i].RequestType == "EXTRA" {
+					creditTransaction = append(creditTransaction, models.CreditTransaction{
+						TransactionCode: GetCreditRes.(ResultCredit).Credit[0].CreditExtra[0].ID.String(),
+						TransactionType: "EXTRA",
+						Amount:          GetCreditRes.(ResultCredit).Credit[0].CreditExtra[0].Amount,
+						AdjustAmount:    0,
+						EffectiveDtm:    GetCreditRes.(ResultCredit).Credit[0].CreditExtra[0].EffectiveDtm,
+						ExpireDtm:       GetCreditRes.(ResultCredit).Credit[0].CreditExtra[0].ExpireDtm,
+						IsApprove:       false,
+						Status:          "REJECT",
+						Reason:          "",
+					})
+					req[0].RequestCode = GetCreditRes.(ResultCredit).Credit[0].CreditExtra[0].DocRef
+					creditExtraIDForDelete = append(creditExtraIDForDelete, GetCreditRes.(ResultCredit).Credit[0].ID)
+				} else {
+					creditTransaction = append(creditTransaction, models.CreditTransaction{
+						TransactionCode: GetCreditRes.(ResultCredit).Credit[0].ID.String(),
+						TransactionType: "EXTRA",
+						Amount:          GetCreditRes.(ResultCredit).Credit[0].Amount,
+						AdjustAmount:    0,
+						IsApprove:       false,
+						Status:          "REJECT",
+						Reason:          "",
+					})
+					req[0].RequestCode = GetCreditRes.(ResultCredit).Credit[0].DocRef
+					creditIDForDelete = append(creditIDForDelete, GetCreditRes.(ResultCredit).Credit[0].ID)
+				}
+			}
 
-		} */
+		}
 		if req[i].Status == "COMPLETED" {
 			creditExtra := []models.CreditExtra{}
 			CreditID := uuid.New()
@@ -75,19 +120,19 @@ func UpdateCreditRequest(ctx *gin.Context, jsonPayload string) (interface{}, err
 		}
 
 		creditRequestValue = append(creditRequestValue, req[i])
-
-		creditTransaction = append(creditTransaction, models.CreditTransaction{
-			TransactionCode: req[i].RequestCode,
-			TransactionType: req[i].RequestType,
-			Amount:          req[i].Amount,
-			AdjustAmount:    0,
-			EffectiveDtm:    req[i].EffectiveDtm,
-			ExpireDtm:       req[i].ExpireDtm,
-			IsApprove:       false,
-			Status:          req[i].Status,
-			Reason:          "",
-		})
-
+		if req[i].Status != "REJECT" {
+			creditTransaction = append(creditTransaction, models.CreditTransaction{
+				TransactionCode: req[i].RequestCode,
+				TransactionType: req[i].RequestType,
+				Amount:          req[i].Amount,
+				AdjustAmount:    0,
+				EffectiveDtm:    req[i].EffectiveDtm,
+				ExpireDtm:       req[i].ExpireDtm,
+				IsApprove:       false,
+				Status:          req[i].Status,
+				Reason:          "",
+			})
+		}
 	}
 	if len(creditTransaction) > 0 {
 		jsonByteserrCreditTransaction, err := json.Marshal(creditTransaction)
@@ -97,6 +142,12 @@ func UpdateCreditRequest(ctx *gin.Context, jsonPayload string) (interface{}, err
 		_, errCreditTransaction := CreateCreditTransaction(ctx, string(jsonByteserrCreditTransaction))
 		if errCreditTransaction != nil {
 			return nil, errCreditTransaction
+		}
+	}
+	if len(creditIDForDelete) > 0 || len(creditExtraIDForDelete) > 0 {
+		errDeleteCredit := repositoryCredit.DeleteCredit(creditIDForDelete, creditExtraIDForDelete)
+		if errDeleteCredit != nil {
+			return nil, errDeleteCredit
 		}
 	}
 
