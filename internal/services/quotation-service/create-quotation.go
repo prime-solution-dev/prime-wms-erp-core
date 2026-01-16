eb0a39977c1b28f21e5ea51d67e92d2292b6e785
Theo <theoseathan@gmail.com>
2026-01-16 14:19:00 +0700

    edit check quotation alert

--- internal/services/quotation-service/create-quotation.go
+++ internal/services/quotation-service/create-quotation.go
@@ -141,6 +141,10 @@ func CreateQuotation(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 			DocRef:             tempQuotation.QuotationCode,
 			CustomerCode:       quotationReq.CustomerCode,
 			EffectiveDatePrice: *quotationReq.EffectiveDatePrice,
+			TransportCost:      quotationReq.TotalTransportCost,
+			TransportType:      quotationReq.TransportCostType,
+			TotalAmount:        quotationReq.SubtotalExclVat,
+			TotalWeight:        quotationReq.TotalWeight,
 			Items:              []verifyService.VerifyApproveItem{},
 		}
 
@@ -168,7 +172,7 @@ func CreateQuotation(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 				TotalWeight:   item.TotalWeight,
 				PriceUnit:     item.PriceUnit,
 				PriceListUnit: item.PriceListUnit,
-				TotalAmount:   item.TotalAmount,
+				TotalAmount:   item.SubtotalExclVat,
 				SaleUnit:      item.SaleUnit,
 				SaleUnitType:  item.SaleUnitType,
 			}
