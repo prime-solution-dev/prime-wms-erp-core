b4eacadfff29e92b12b4fdb8f2aa2d9af2281207
Theo <theoseathan@gmail.com>
2026-01-30 23:18:57 +0700

    update sale item status

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -186,6 +186,10 @@ func RegisterRoutes(ctx *gin.Engine) {
 	sale.POST("/ValidateSaleOrder", func(c *gin.Context) {
 		utils.ProcessRequest(c, saleService.ValidateSale)
 	})
+
+	sale.POST("/UpdateSaleItemStatus", func(c *gin.Context) {
+		utils.ProcessRequest(c, saleService.UpdateSaleItemStatus)
+	})
 	//delivery
 	delivery := ctx.Group("/delivery")
 	delivery.POST("/CreateDelivery", func(c *gin.Context) {
