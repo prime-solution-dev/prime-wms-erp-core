c5d5ad00b9a06a0c801019607076d5fb2435ce3c
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2026-01-31 17:50:58 +0700

    re

--- internal/repositories/credit/repository.go
+++ internal/repositories/credit/repository.go
@@ -287,16 +287,16 @@ func GetCreditRequestPreload(id []uuid.UUID, customerCode []string, isAction []b
 		Where("1=1").
 		Group("customer_code").Find(&creditRequest).Error
 
-	err = gormx.Model(&models.Credit{}).
-		Select("credit.customer_code, SUM(credit.amount) AS amount, SUM(credit_extra.amount) AS temporary_increase_credit_limit, " +
-			"MIN(credit_extra.effective_dtm) AS effective_dtm, " +
-			"MAX(credit_extra.expire_dtm) AS expire_dtm, " +
-			"CASE " +
-			"WHEN EXISTS (" +
-			"SELECT 1 FROM credit_request cr WHERE cr.customer_code = credit.customer_code AND cr.status = 'PENDING'" +
-			") THEN true ELSE false END AS is_approve ").
-		Joins("LEFT JOIN credit_extra ON credit_extra.credit_id = credit.id").
-		Group("credit.customer_code").Find(&creditRequest).Error
+	/* 	err = gormx.Model(&models.Credit{}).
+	Select("credit.customer_code, SUM(credit.amount) AS amount, SUM(credit_extra.amount) AS temporary_increase_credit_limit, " +
+		"MIN(credit_extra.effective_dtm) AS effective_dtm, " +
+		"MAX(credit_extra.expire_dtm) AS expire_dtm, " +
+		"CASE " +
+		"WHEN EXISTS (" +
+		"SELECT 1 FROM credit_request cr WHERE cr.customer_code = credit.customer_code AND cr.status = 'PENDING'" +
+		") THEN true ELSE false END AS is_approve ").
+	Joins("LEFT JOIN credit_extra ON credit_extra.credit_id = credit.id").
+	Group("credit.customer_code").Find(&creditRequest).Error */
 
 	sqlDB, err1 := gormx.DB()
 	if err1 != nil {
