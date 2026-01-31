150e517204c9be93f38e3cf1784fb04a1bcb97d7
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2026-01-31 18:18:09 +0700

    creditRequestUpdate

--- internal/services/cronjob-service/cron-kernal-data.go
+++ internal/services/cronjob-service/cron-kernal-data.go
@@ -22,11 +22,15 @@ func GetKernal() {
 	ttt := "start kernal service"
 	println(ttt)
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		CreditRequestEffectiveDtmPending()
+	}()
 
 	go func() {
 		defer wg.Done()
-		//CreditRequestEffectiveDtm()
+		CreditRequestEffectiveDtm()
 	}()
 
 	go func() {
