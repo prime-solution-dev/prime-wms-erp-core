1ab403e395d760fba57ae6dbdbb1aeffb67d800a
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2026-03-11 23:52:05 +0700

    Deposit

--- internal/services/invoice-service/update-invoice-ar.go
+++ internal/services/invoice-service/update-invoice-ar.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	models "prime-erp-core/internal/models"
+	depositService "prime-erp-core/internal/services/deposit-service"
 	interfaceService "prime-erp-core/internal/services/interface-service"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -51,6 +53,51 @@ func UpdateInvoiceAR(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 		if err != nil {
 			return nil, err
 		}
+	}
+	depositMapResult, err := interfaceService.GetDeposit(req[0].ExternalID)
+	if err != nil {
+		return nil, err
+	}
+	if len(depositMapResult) > 0 {
+		var deposit []models.Deposit
+
+		for _, v := range depositMapResult {
+			depMap, _ := v.(map[string]interface{})
+
+			totalFloat, err := strconv.ParseFloat(depMap["total"].(string), 64)
+			if err != nil {
+				totalFloat = 0
+			}
+			drFloat, err := strconv.ParseFloat(depMap["dr"].(string), 64)
+			if err != nil {
+				totalFloat = 0
+			}
+			crFloat, err := strconv.ParseFloat(depMap["cr"].(string), 64)
+			if err != nil {
+				totalFloat = 0
+			}
+
+			deposit = append(deposit, models.Deposit{
+				DepositCode:  depMap["anchor"].(string),
+				CustomerCode: req[0].PartyCode,
+				AmountTotal:  totalFloat,
+				AmountUsed:   drFloat,
+				AmountRemain: crFloat,
+				Status:       "PENDING",
+			})
+		}
+		if len(deposit) > 0 {
+			jsonBytesCreateDeposit, err := json.Marshal(deposit)
+			if err != nil {
+				return nil, err
+			}
+
+			_, errDeposit := depositService.CreateDepost(ctx, string(jsonBytesCreateDeposit))
+			if errDeposit != nil {
+				return nil, errDeposit
+			}
+		}
+
 	}
 	return createInvoiceReturn, nil
 
