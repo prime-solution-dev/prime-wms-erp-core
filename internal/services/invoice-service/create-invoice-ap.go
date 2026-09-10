c1b7c5c361cb3dfafe8bba07cddba50f5a7f98b5
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2026-09-10 14:12:35 +0700

    priceUnit

--- internal/services/invoice-service/create-invoice-ap.go
+++ internal/services/invoice-service/create-invoice-ap.go
@@ -301,6 +301,11 @@ func CreateInvoiceAP(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 				req[i].InvoiceItem[it].TotalDiscount = poQTYMapResult.TotalDiscount
 				req[i].InvoiceItem[it].TotalDiscount_percent = poQTYMapResult.TotalDiscountPercent
 				totalBeforeDiscount := 0.0
+				priceUnit, err := calculateAPPriceUnit(
+					poQTYMapResult.UnitUom, req[i].InvoiceItem[it].UnitUom,
+					poQTYMapResult.PriceUnit, invoiceItem.Qty, invoiceItem.Weight,
+				)
+				req[i].InvoiceItem[it].PriceUnit = priceUnit
 				if strings.EqualFold(strings.TrimSpace(poQTYMapResult.PurchaseUnit), "KG") {
 					totalBeforeDiscount = poQTYMapResult.PriceUnit * req[i].InvoiceItem[it].Weight
 				} else {
@@ -309,19 +314,15 @@ func CreateInvoiceAP(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 				req[i].InvoiceItem[it].TotalDiscount = calculateAPDiscount(totalBeforeDiscount,
 					req[i].InvoiceItem[it].TotalDiscount_percent, poQTYMapResult.TotalDiscount)
 				req[i].InvoiceItem[it].SubtotalExclVat = totalBeforeDiscount - req[i].InvoiceItem[it].TotalDiscount
+
 				req[i].InvoiceItem[it].TotalVat = req[i].InvoiceItem[it].SubtotalExclVat * 0.07
 				req[i].InvoiceItem[it].TotalAmount = req[i].InvoiceItem[it].SubtotalExclVat + req[i].InvoiceItem[it].TotalVat
 
 				totalAmount += req[i].InvoiceItem[it].TotalAmount //total cost
 
-				priceUnit, err := calculateAPPriceUnit(
-					poQTYMapResult.UnitUom, req[i].InvoiceItem[it].UnitUom,
-					poQTYMapResult.PriceUnit, invoiceItem.Qty, invoiceItem.Weight,
-				)
 				if err != nil {
 					return nil, fmt.Errorf("PO %s item %s: %w", invoiceItem.DocumentRef, invoiceItem.DocumentRefItem, err)
 				}
-				req[i].InvoiceItem[it].PriceUnit = priceUnit
 
 				//movingAvgCost ใช้ เฉพาะ fab
 				if req[i].InvoiceType == "AP-FAB" {
