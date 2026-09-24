a331dafeb3f2b2aae4a1e83e6ae3b42a3ac28342
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2026-09-24 12:13:22 +0700

    draft don't run

--- internal/services/invoice-service/create-invoice-dn.go
+++ internal/services/invoice-service/create-invoice-dn.go
@@ -38,13 +38,25 @@ func CreateInvoiceDN(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 	}
 	prefix := "DN"
 	configCodeValue := "RUNNING_DN"
-	count := len(req)
-	invoiceCodes, err := GenerateInvoiceCodes(ctx, count, prefix, configCodeValue)
-	if err != nil {
-		return nil, errors.New("failed to generate invoice codes: " + err.Error())
-	}
+	count := 0
 	for i := range req {
-		req[i].InvoiceCode = invoiceCodes[i]
+		if req[i].InvoiceCode == "" {
+			count++
+		}
+	}
+	var invoiceCodes []string
+	if count > 0 {
+		invoiceCodes, err = GenerateInvoiceCodes(ctx, count, prefix, configCodeValue)
+		if err != nil {
+			return nil, errors.New("failed to generate invoice codes: " + err.Error())
+		}
+	}
+	codeIndex := 0
+	for i := range req {
+		if req[i].InvoiceCode == "" {
+			req[i].InvoiceCode = invoiceCodes[codeIndex]
+			codeIndex++
+		}
 		conMapCustomer, exist := convertCustomerMap[req[i].PartyCode]
 		if exist {
 			req[i].PartyName = conMapCustomer.CustomerName
