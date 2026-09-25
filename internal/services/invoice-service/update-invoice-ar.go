b18fba74fd177771d55e25ceabf23b90b9da3069
WuttichaiChamp <wuttichai.p@prime-solution.cc>
2026-09-25 16:51:01 +0700

    reqHook

--- internal/services/invoice-service/update-invoice-ar.go
+++ internal/services/invoice-service/update-invoice-ar.go
@@ -88,7 +88,11 @@ func UpdateInvoiceAR(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 		for _, hookConfigValue := range hookConfig {
 			urlHook = hookConfigValue.HookUrl
 		}
-		reqHook := req
+
+		var reqHook []models.Invoice
+		if err := json.Unmarshal(jsonBytesCreateInvoice, &reqHook); err != nil {
+			return nil, errors.New("failed to copy invoice request for hook: " + err.Error())
+		}
 		productCodes := []string{}
 		for i := range reqHook {
 			for it := range reqHook[i].InvoiceItem {
@@ -120,7 +124,7 @@ func UpdateInvoiceAR(ctx *gin.Context, jsonPayload string) (interface{}, error) 
 		}
 
 		requestDataCreateHook := interfaceService.HookInterfaceRequest{
-			RequestData: req,
+			RequestData: reqHook,
 			UrlHook:     urlHook,
 		}
 		_, err := interfaceService.HookInterface(requestDataCreateHook)
